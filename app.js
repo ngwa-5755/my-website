f15d1cc704690766b251d03733fb354bf8dcc335
ngwa-5755 <155694062+ngwa-5755@users.noreply.github.com>
2024-07-21 00:29:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -96,6 +96,6 @@ app.get('/auth/logout', (req, res) => {
   res.redirect('/');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, ('0.0.0.0') => {
   console.log(`Server is running on port ${PORT}`);
 });
